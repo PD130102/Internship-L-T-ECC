8a8a5787e2dfa336846ea974bb65d5ebcd68e20f
Pranav Deepak CB.EN.U4CSE20346 <cb.en.u4cse20346@cb.students.amrita.edu>
2022-08-12 15:28:10 +0530

    Initial Commit

---
+++ Internship/Services/CompanyAPIService.cs
@@ -0,0 +1,95 @@
+﻿using Internship.Models;
+using System.Collections.Generic;
+
+
+namespace Internship.Services
+{
+    public static class CompanyAPIService
+    {
+        static List<CompanyAPI> companies { get; }
+        static CompanyAPIService()
+        {
+            companies = new List<CompanyAPI> { };
+            {
+                new CompanyAPI
+                {
+                    CompanyName = "L&T Construction",
+                    CompanyCode = 1,
+                    Sectors = new List<Sector>
+                    {
+                        new Sector
+                        {
+                            SectorID = 1,
+                            SectorName = "Airport",
+                            SectorDescription = "Airport are Huge and Beautiful",
+                            Skills = new List<string> { "architect", "engineer", "construction crew", "manager" },
+                            Projects = new List<string>{"Delhi International Airport – T3","Hyderabad International Airport","Kannur International Airport","Salalah International Airport", "Oman","Abu Dhabi International Airport Complex"},
+                            Atributes = new List<string>{"Hello","World"},
+                            Factors = new List<string>{"Good","Time"},
+                            Materials = new List<string>{ "iron", "carbon", "sand", "concrete" }
+                        },
+                        new  Sector
+                        {
+                            SectorID = 2,
+                            SectorName = "Bridge",
+                            SectorDescription = "Bridges are Huge and Beautiful",
+                            Skills = new List<string> { "architect", "engineer", "construction crew", "manager" },
+                            Projects = new List<string>{"Delhi International Airport – T3","Hyderabad International Airport","Kannur International Airport","Salalah International Airport", "Oman","Abu Dhabi International Airport Complex"},
+                            Atributes = new List<string>{"Hello","World"},
+                            Factors = new List<string>{"Good","Time"},
+                            Materials = new List<string>{ "iron", "carbon" }
+                        }
+                    }
+
+                };
+                new CompanyAPI
+                {
+                    CompanyName = "L&T Infotech",
+                    CompanyCode = 2,
+                    Sectors = new List<Sector>
+                    {
+                        new Sector
+                        {
+                            SectorID = 1,
+                            SectorName = "Airport",
+                            SectorDescription = "Airport are Huge and Beautiful",
+                            Skills = new List<string> { "architect", "engineer", "construction crew", "manager" },
+                            Projects = new List<string>{"Delhi International Airport – T3","Hyderabad International Airport","Kannur International Airport","Salalah International Airport", "Oman","Abu Dhabi International Airport Complex"},
+                            Atributes = new List<string>{"Hello","World"},
+                            Factors = new List<string>{"Good","Time"},
+                            Materials = new List<string>{ "iron", "carbon", "sand", "concrete" }
+                        },
+                        new  Sector
+                        {
+                            SectorID = 2,
+                            SectorName = "Bridge",
+                            SectorDescription = "Bridges are Huge and Beautiful",
+                            Skills = new List<string> { "architect", "engineer", "construction crew", "manager" },
+                            Projects = new List<string>{"Delhi International Airport – T3","Hyderabad International Airport","Kannur International Airport","Salalah International Airport", "Oman","Abu Dhabi International Airport Complex"},
+                            Atributes = new List<string>{"Hello","World"},
+                            Factors = new List<string>{"Good","Time"},
+                            Materials = new List<string>{ "iron", "carbon" }
+                        }
+                    }
+
+                };
+            }
+        }
+
+        public static List<CompanyAPI> GetAll()
+        {
+            return companies;
+        }
+
+        public static CompanyAPI Get(int id)
+        {
+            CompanyAPI? p = companies.Find(p => p.CompanyCode == id);
+            return p ?? throw new Exception("Companies not Found");
+        }
+
+        public static void Add(CompanyAPI company)
+        {
+            companies.Add(company);
+        }
+    }
+}
